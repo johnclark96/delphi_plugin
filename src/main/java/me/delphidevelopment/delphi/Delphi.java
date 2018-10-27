b19a4c796b23962596d78e46c3441e496b4ed7c2
Delphi Systems <mlitvinkox96@gmail.com>
2018-10-26 23:12:44 -0400

    Initial commit

---
+++ src/main/java/me/delphidevelopment/delphi/Delphi.java
@@ -0,0 +1,17 @@
+package me.delphidevelopment.delphi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Delphi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
